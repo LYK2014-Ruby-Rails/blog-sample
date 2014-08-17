7bb54e5dfcb5902c5f98b5cce07ca8efd0f49b01
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-08-17 16:18:03 +0300

    Article create options was done  - Article controller was added  - Article model and migration files were created  - New article form was created

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,23 @@
+class ArticlesController < ApplicationController
+
+  def new
+  end
+
+
+  def create
+
+    @article = Article.new(article_params)
+
+    @article.save
+
+    redirect_to @article
+
+  end
+
+private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :articles
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
