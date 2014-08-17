bf6203e48227ae33531b04c711b964b805807b2b
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-08-17 17:37:53 +0300

    Article validations were added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,9 +16,12 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
 
-    redirect_to @article
 
   end
 
