3f5a12c680198a2b7ad19d7a487495565f416ae5
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-08-17 19:52:52 +0300

    Article destroy options were done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,14 @@ class ArticlesController < ApplicationController
       render 'new'
     end
 
+  end
 
+  def destroy
+
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
 private
