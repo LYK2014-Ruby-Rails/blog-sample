ace2d123d067b20e563cbac667533094d2909aef
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-08-18 11:48:57 +0300

    Comment model and Article-Comment associations were created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
+  has_many :comments
+
   validates :title, presence: true,
             length: { minimum: 5 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817122829) do
+ActiveRecord::Schema.define(version: 20140818083621) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20140817122829) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
