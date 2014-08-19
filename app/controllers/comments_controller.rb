d0bcd348e374076541b599c83b6c6db8c220847c
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2014-08-19 11:37:45 +0300

    HTTP Basic Auth was implemented

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: 'lyk2014', password: 'secret', only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
